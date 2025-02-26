b53fd47ebe47229ebe092f70c567708c8911fd17
volmodaoist <1304548501@qq.com>
2025-02-26 11:07:43 +0800

    feat: 支持子序列约束搜索

--- TraceSpanTree/MultiNestDict.py
+++ TraceSpanTree/MultiNestDict.py
@@ -31,19 +31,29 @@ class MultiNestDict:
         return data
 
     @staticmethod
-    def find_key(data:Dict, target_key:str, default:Any = None) -> Any:
-        def recursive_search(d):
+    def find_key(data:Dict, target_key:str, default:Any = None, sep = '.') -> Any:
+        ''' target_key 可以是多个key通过'.'连接在一起的序列，当这个子序列是根节点到目标节点的子序列时，
+            能够实现缩小目标 key 范围的效果 (通常用于目标key存在同名的情况)
+        ''' 
+        def recursive_search(d, target_key):
             if isinstance(d, (dict, list)):
                 iterable = d.items() if isinstance(d, dict) else d
                 for item in iterable:
                     k, v = item if isinstance(d, dict) else (None, item)
                     if k == target_key:
                         return v
-                    result = recursive_search(v)
+                    result = recursive_search(v, target_key)
                     if result is not None:
                         return result
             return None
-        return recursive_search(MultiNestDict.expand(data)) or default
+        
+        data, target_key_parts = MultiNestDict.expand(data), target_key.split(sep)
+        for part in target_key_parts:
+            val = recursive_search(data, part)
+            data = val
+            if data is None:
+                break
+        return val or default
     
     @staticmethod
     def update_key(data:Dict, target_key:str, val) -> bool:
@@ -103,7 +113,11 @@ if __name__ == '__main__':
                 {'key4': 'value4'},
                 {'key5': 'value5'}
             ],
-            'level6': '{"key6": "value6"}' 
+            'level6': '{"key6": "value6"}',
+            'level5': {
+                'key7-1': {'key8': 'k-78'},
+                'key7-2': {'key8': 'k-88'},
+            }
         }
     }
 
@@ -114,7 +128,10 @@ if __name__ == '__main__':
     print(v)
     v = MultiNestDict.find_key(nested_data, "key6")
     print(v)
-    
+    v = MultiNestDict.find_key(nested_data, "key7-1.key8")
+    print(v)
+    v = MultiNestDict.find_key(nested_data, "key7-2.key8")
+    print(v)
     
     # NOTE 更新key-value 
     MultiNestDict.update_key(nested_data, target_key="level6", val = 'newval')
